d7e43c8e556cb44b04bd3909795613f6debc262b
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-08-03 12:02:52 +0200

    changes made...

---
+++ admin/chapters.php
@@ -0,0 +1,39 @@
+<?php
+include("sessions.php");
+include("connection.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My-Bible App</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./Images/icon.webp" type="image/x-icon">
+    <script src="./sweetalert.min.js"></script>
+</head>
+<body>
+
+    <div class="dashboard-container">
+
+        <!-- Sidebar Starts Here -->
+        <?php include("sidebar.php");?>
+        <!-- Sidebar Ends Here -->
+
+        <!-- Container Starts Here -->
+        <div class="container">
+
+            <div class="title">
+                <h1>[My-Bible App] - Chapters</h1>
+            </div>
+
+            <div class="content">
+            </div>
+
+        </div>
+        <!-- Container Ends Here -->
+
+    </div>
+</body>
+</html>
